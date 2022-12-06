4f562d96480b1d4c9d0f9a68af2011034f5fcbdd
Selcuk ARSLAN <slckarslan93@gmail.com>
2022-12-06 16:27:06 +0300

    Modified

--- DataAccess/Paging/Paginate.cs
+++ DataAccess/Paging/Paginate.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataAccess.Paging
+﻿namespace DataAccess.Paging
 {
     public class Paginate<T> : IPaginate<T>
     {
--- DataAccess/Repositories/Repository.cs
+++ DataAccess/Repositories/Repository.cs
@@ -3,13 +3,7 @@ using DataAccess.Paging;
 using Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace DataAccess.Repositories
 {
